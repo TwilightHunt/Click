98e4ccf2d239cfac4da70411b85b1e72e850b417
Creator God69 <rainbowdeity70@gmail.com>
2020-01-26 19:24:33 +0500

    Теперь круги появляются рандомно в разных местах

--- Click/Assets/Scripts/SpawnScript.cs
+++ Click/Assets/Scripts/SpawnScript.cs
@@ -1,20 +1,51 @@
-﻿using UnityEngine;
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
 
 public class SpawnScript : MonoBehaviour
 {
-    public Vector3 center; // координаты центра
-    public Vector3 size; // координаты в которых будут появляться объекты
- 
-    public GameObject sphere; // сфера
+    public RectTransform SpawnArea;
+    public GameObject Sphere;
+    public Vector2 MinMaxSphereSize = new Vector2(0.2f, 3f);
 
-    void Start()
+    [Header("Gizmos")]
+    public float GizmosRadius = 2f;
+    public Color GizmosColor = Color.blue;
+    private Vector3[] corners = new Vector3[4];
+
+
+    private void OnDrawGizmos()
     {
-        Spawn();
+        if (SpawnArea == null)
+            return;
+
+        Gizmos.color = GizmosColor;        
+        SpawnArea.GetWorldCorners(corners);
+        foreach(Vector3 pos in corners)
+        {
+            Gizmos.DrawSphere(pos, GizmosRadius);
+        }
+
+    }
+
+    private void Start()
+    {
+        //https://docs.unity3d.com/ScriptReference/RectTransform.GetWorldCorners.html
+        SpawnArea.GetWorldCorners(corners);
+
+        for (int i = 0; i < 25; i++)
+        {
+            Spawn();
+        }
     }
 
     public void Spawn()
     {
-        Vector3 pos2 = center + new Vector3(Random.Range(-size.x / 2, size.x / 2), Random.Range(-size.y / 2, size.y / 2), Random.Range(-size.z / 2, size.z / 2));
-        Instantiate(sphere, pos2, Quaternion.identity);
+        Vector3 randomPosition = Vector3.zero;
+        randomPosition.x = Random.Range(corners[0].x, corners[3].x);
+        randomPosition.y = Random.Range(corners[0].y, corners[1].y);
+
+        GameObject sphere = Instantiate(Sphere, randomPosition, Quaternion.identity, transform);
+        sphere.transform.localScale = Vector3.one * Random.Range(MinMaxSphereSize.x, MinMaxSphereSize.y);
     }
 }
